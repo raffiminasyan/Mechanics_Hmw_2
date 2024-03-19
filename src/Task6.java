6a8321a727d56dd8f52a26b80d5128e8698c5bbb
raffiminasyan <raffiminasyan@gmail.com>
2024-03-19 21:06:04 +0400

    Description of the changes made

--- src/Task6.java
+++ src/Task6.java
@@ -4,10 +4,9 @@ public class Task6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input parameters
         double M1, M2, M3, u1, u2, u3, L, dt;
 
-        // Input masses
+
         System.out.print("Enter mass of M1 (kg): ");
         M1 = scanner.nextDouble();
         System.out.print("Enter mass of M2 (kg): ");
@@ -15,7 +14,7 @@ public class Task6 {
         System.out.print("Enter mass of M3 (kg): ");
         M3 = scanner.nextDouble();
 
-        // Input initial velocities
+
         System.out.print("Enter initial velocity of M1 (m/s): ");
         u1 = scanner.nextDouble();
         System.out.print("Enter initial velocity of M2 (m/s): ");
@@ -23,37 +22,37 @@ public class Task6 {
         System.out.print("Enter initial velocity of M3 (m/s): ");
         u3 = scanner.nextDouble();
 
-        // Input rope length
+
         System.out.print("Enter rope length (m): ");
         L = scanner.nextDouble();
 
-        // Input time step
+
         System.out.print("Enter time step (s): ");
         dt = scanner.nextDouble();
 
-        // Simulation
-        double D_max = 0; // Max displacement of M1
-        double a1, a2, a3; // Accelerations
-        double x1 = 0; // Initial displacement of M1
+
+        double D_max = 0;
+        double a1, a2, a3;
+        double x1 = 0;
 
         while (true) {
-            // Calculate accelerations
+
             a1 = (M2 * u2 * u2 - M3 * u3 * u3) / (M1 + M2 + M3);
             a2 = (u1 * u1 - u3 * u3) / L;
             a3 = (2 * M2 * u2 * u2 - M1 * u1 * u1) / (M1 + M2 + M3);
 
-            // Update velocities
+
             u1 += a1 * dt;
             u2 += a2 * dt;
             u3 += a3 * dt;
 
-            // Update displacements
+
             x1 += u1 * dt;
             if (x1 > D_max) {
                 D_max = x1;
             }
 
-            // Check if M3 hits the ground
+
             if (u3 <= 0) {
                 break;
             }
